59333a86da831978cecbd08d28be54ec603aae0e
hung99ktb <hung99ktb@gmail.com>
2025-07-02 16:42:55 +0700

    Debug: check jwt response

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -16,8 +16,13 @@ export class JwtAuthGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     const jwt =
       context.switchToHttp().getRequest().cookies?.Authentication ||
-      context.switchToHttp().getRequest().headers?.cookie.split("=")[1] ||  // ??? Cân tìm hiểu format của cookie
+      context.switchToHttp().getRequest().headers?.cookie?.split("=")[1] ||  // ??? Cân tìm hiểu format của cookie
       context.switchToHttp().getRequest().headers?.Authentication;
+
+    console.log(jwt);
+    console.log(context.switchToHttp().getRequest().headers);
+    console.log(context.switchToHttp().getRequest().cookies);
+    
     if (!jwt) {
       return false;
     }
